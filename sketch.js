a52903d29f07eb4436181f49069b6f06055ce4cd
fesedrani <fesedrani@gmail.com>
2021-12-09 18:33:26 -0300

    de paltaform para plataforma

--- sketch.js
+++ sketch.js
@@ -4,7 +4,9 @@ const Bodies = Matter.Bodies;
 
 var engine, world;
 var box1, pig1;
-var backgroundImg,platform;
+var backgroundImg;
+var plataforma
+
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -17,7 +19,7 @@ function setup(){
 
 
     ground = new Ground(600,height,1200,20);
-    platform = new Ground(150, 305, 300, 170);
+    plataforma = new Ground(150, 305, 300, 170);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -60,5 +62,5 @@ function draw(){
     log5.display();
 
     bird.display();
-    platform.display();
+    plataforma.display();
 }
